3181759ce8a5bda9c96dc867347337c8b1eb6220
S1M3ONS4YS <97121362+S1M3ONS4YS@users.noreply.github.com>
2024-01-18 21:36:38 -0800

    Created three new files showcasing Runtime Errors and Syntax Errors

---
+++ Java/Introduction_To_Java/Chapter1/src/ShowSyntaxErrors.java
@@ -0,0 +1,7 @@
+public class ShowSyntaxErrors {
+    //The void keyword is missing
+    public static main(String[] args){
+        //The string is not properly closed
+        System.out.println("Welcome to Java);
+    }
+}
