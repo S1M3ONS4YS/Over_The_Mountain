b6cc93d3e9f86750e502d6897db32cecab412b21
S1M3ONS4YS <97121362+S1M3ONS4YS@users.noreply.github.com>
2024-02-09 13:55:46 -0800

    Created and solved three exercises from the textbook

--- Java/Introduction_To_Java/Chapter1/src/ShowSyntaxErrors.java
+++ Java/Introduction_To_Java/Chapter1/src/ShowSyntaxErrors.java
@@ -1,7 +1,7 @@
 public class ShowSyntaxErrors {
     //The void keyword is missing
-    public static main(String[] args){
+    public static void main(String[] args){
         //The string is not properly closed
-        System.out.println("Welcome to Java);
+        System.out.println("Welcome to Java");
     }
 }
